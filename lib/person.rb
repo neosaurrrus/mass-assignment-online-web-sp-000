7896818c8ce486f7ddcafb6e2b6ccbe79f63111a
Lukie Kang <lukiek+f@gmail.com>
2019-07-01 06:15:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 
-  def intialize(attributes)
+  def initialize(attributes)
     attributes.each { | key, value |
     self.send(("#{key}="), value)
     }
